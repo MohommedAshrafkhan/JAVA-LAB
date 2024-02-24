20a8b5d95db6475fcc63a3f6f56055a1247caf03
MohommedAshrafkhan <161120136+MohommedAshrafkhan@users.noreply.github.com>
2024-02-24 15:22:16 +0530

    add,sub,mul,div

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+class helloworld{
+	public static void main(String[] args){
+		int a=3,b=4;
+		System.out.println("result is "+(a+b));
+		System.out.println("result is "+(a-b));
+		System.out.println("result is "+(a*b));
+		System.out.println("result is "+(a/b));
+	}
+}
+
+	

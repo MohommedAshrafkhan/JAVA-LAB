d13680525606e86eb81f3653f7391f977ae4b2de
Ashraf Khan <161120136+MohommedAshrafkhan@users.noreply.github.com>
2024-04-20 15:31:51 +0530

    arithmetic operations done by creating our own package

---
+++ addsubimpl.java
@@ -0,0 +1,9 @@
+import arithmeticop.*;
+class addsubimpl{
+	public static void main(String[] args){
+		addition a1=new addition(4,3);
+		a1.sum();
+		substraction s1=new substraction(5,4);
+		s1.diffrence();
+	}
+}
---
+++ substraction.java
@@ -0,0 +1,12 @@
+package arithmeticop;
+public class substraction{
+	private int d1,d2;
+	public substraction(int n1,int n2){
+		this.d1=n1;
+		this.d2=n2;
+	}
+	public void diffrence()
+	{
+    		System.out.println("diffrence of ntwo given numbers is"+(d1-d2));
+	}
+}
